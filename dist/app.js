573c074bcb6c89d60b91dabff435dee290b020be
ashik <asikk2925@gmail,com>
2026-08-24 10:15:12 +0600

    new update

--- dist/app.js
+++ dist/app.js
@@ -25,7 +25,14 @@ app.use(cors({
         ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "X-Custom-Header",
+        "better-auth-agent",
+        "better-auth-csrf"
+    ],
 }));
 app.use(helmet());
 app.use(morgan("dev"));
--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,15 @@ app.use(
       ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    
+    allowedHeaders: [
+      "Content-Type", 
+      "Authorization", 
+      "X-Requested-With", 
+      "X-Custom-Header", 
+      "better-auth-agent", 
+      "better-auth-csrf"
+    ],
   })
 );
 
